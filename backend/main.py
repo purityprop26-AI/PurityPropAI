e21b29d9043d80d75817bc38563349bafb49478d
Naveen kumar T <naveenkumart949@gmail.com>
2026-02-02 03:40:59 +0530

    Change allow_credentials to False

--- backend/main.py
+++ backend/main.py
@@ -21,7 +21,7 @@ app.add_middleware(
         "https://purity-prop-f-git-main-naveens-projects-36f95ce0.vercel.app"    # frontend on vercel
         "https://purity-prop-f.vercel.app"
     ],
-    allow_credentials=True,
+    allow_credentials=False,
     allow_methods=["*"],
     allow_headers=["*"],
 )
